941c546407cd022ccd3828b6725cd5541ad11516
May Myat Moe <122775995+May-Moe@users.noreply.github.com>
2025-04-21 09:10:53 +0700

    delete shework

--- recyglolms/home.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-home_bp = Blueprint('main', __name__)
-
-@home_bp.route('/')
-def homepage():
-    return render_template('shework.html')  # Just shows the homepage
